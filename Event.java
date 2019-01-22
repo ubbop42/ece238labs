1b5151f9b53e6be8fcc5b7ca6e14de6acf48f237
UMESH DHURVAS <udhurvas@uwaterloo.ca>
2019-01-22 18:03:48 -0500

    event list

---
+++ Event.java
@@ -0,0 +1,9 @@
+public class Event {
+    String type;
+    double time;
+
+    Event(String type, double time) {
+        this.type = type;
+        this.time = time;
+    }
+}
--- lab1.java
+++ lab1.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
-public class lab1{
-
-     public static void main(String []args){
+public class lab1 {
+    public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-         
+
         System.out.print("enter 0 for mm1 or 1 mm1k:");
         boolean isbounded = scanner.nextInt() == 1;
-        
+
         // Observer events = rand(n)
         System.out.print("n?: ");
         double n = scanner.nextDouble();
@@ -25,39 +24,63 @@ public class lab1{
         double c = scanner.nextDouble();
 
         // Simulation length
-        System.out.print("tme?: ");
+        System.out.print("time?: ");
         double t = scanner.nextDouble();
 
-        if(isbounded){
+        if (isbounded) {
             System.out.print("k?: ");
             double k = scanner.nextDouble();
-            //simulate mm1k
+            // simulate mm1k
+        } else {
+            LinkedList<Event> eventList = new LinkedList<Event>();
+            Double qDelay = 0.0;
+            double currentTime = 0.0;
+            long count = 0;
+            while (currentTime < t) {
+                double delta = genarateRandom(lambda);
+                currentTime += delta;
+                double serviceTime = genarateRandom(1.0 / l) / c;
+                qDelay = Math.max(0, qDelay - delta);
+                if (qDelay > 0)
+                    count++;
+                double departureTime = currentTime + serviceTime + qDelay;
+                qDelay += serviceTime;
+                Event arrival = new Event("Arrival", currentTime);
+                eventList.add(arrival);
+                Event departure = new Event("Departure", departureTime);
+                eventList.add(departure);
+            }
+            System.out.println("events size : " + eventList.size());
+            System.out.println("n of q delayes : " + count);
+            currentTime = 0.0;
+            while (currentTime < t) {
+                currentTime += genarateRandom(lambda * n);
+                Event temp = new Event("Observer", currentTime);
+                eventList.add(temp);
+            }
+            System.out.println("events size : " + eventList.size());
         }
-        else{
-            //simulate mm1
-        }
-        
-        double sum = 0;
-        double nums[] = new double[1000];
-        for(int i = 0; i < 1000;i++){
-            double rand = genarateRandom(lambda);
-            sum += rand;
-            nums[i] = rand;
-        }
-        
-        double avg = sum/1000;
-        double stdev = 0;
-        for(int i = 0 ; i < 1000; i++){
-            stdev += Math.pow(nums[i] - avg, 2);
-        }
-        System.out.println(avg);
-        System.out.println(Math.sqrt(stdev/999));
-        
-        
-     }
-     
-     public static double genarateRandom(double lambda){
-         return -Math.log(1.0-Math.random())/lambda;
-     }
-     
+
+        // double sum = 0;
+        // double nums[] = new double[1000];
+        // for (int i = 0; i < 1000; i++) {
+        // double rand = genarateRandom(lambda);
+        // sum += rand;
+        // nums[i] = rand;
+        // }
+
+        // double avg = sum / 1000;
+        // double stdev = 0;
+        // for (int i = 0; i < 1000; i++) {
+        // stdev += Math.pow(nums[i] - avg, 2);
+        // }
+        // System.out.println(avg);
+        // System.out.println(Math.sqrt(stdev / 999));
+
+    }
+
+    public static double genarateRandom(double lambda) {
+        return -Math.log(1.0 - Math.random()) / lambda;
+    }
+
 }
