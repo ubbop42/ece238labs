df926ff895204cb46f942ec9217c493e3979ae76
Lucas Chevalier <ljcheval@uwaterloo.ca>
2019-03-08 15:31:37 -0500

    add non-persistant

--- lab2.java
+++ lab2.java
@@ -13,7 +13,10 @@ public class lab2 {
         double d = 10; //distance
         double s = 200000000; //propspeed
 
+        System.out.printf("\nPersistant:\n");
         persistant(a,t,r,l,d,s,n);
+        System.out.printf("\n\nNon Persistant:\n");
+        nonPersistant(a,t,r,l,d,s,n);
     }
 
     public static void persistant(double a, double t, double r, double l, double d , double s,int n) {
@@ -58,7 +61,7 @@ public class lab2 {
                 // Check for collision
                 if(currentNode.get(0) < dangertime){
                     collisionCounters[i]++;
-                    double backOffTime = bitTime * generateRandomBackoff((int)Math.pow(2,collisionCounters[i]));
+                    double backOffTime = bitTime * generateRandomBackoff(collisionCounters[i]);
                     
                     //Check if collision count exceeds limit
                     if(collisionCounters[i]<=10){
@@ -66,7 +69,7 @@ public class lab2 {
                         transmissionAttempts++;
                         currentNode.set(0, (currentTime + backOffTime));
                         
-                        //
+                        // Delay other packets in the queue that arrive during backoff
                         for (int j = 1; j < currentNode.size(); j++) {
                             if(currentNode.get(j) < (currentTime + backOffTime)){
                                 currentNode.set(j, (currentTime + backOffTime));
@@ -84,22 +87,24 @@ public class lab2 {
                 }
             }
 
+            // Handle collision
             if(collisionDetected){
-                collisionCounters[currNode]++;
-                double backOffTime = bitTime * generateRandomBackoff((int)Math.pow(2,collisionCounters[currNode]));
+                collisionCounters[currNode]++;	//increment counter
+                double backOffTime = bitTime * generateRandomBackoff(collisionCounters[currNode]); // set backoff
                 if(collisionCounters[currNode] < 10){
-                	double delta = currentTime + backOffTime;
-                    transmittingNode.set(0, delta);
+                	double waitTime = currentTime + backOffTime;
+                    transmittingNode.set(0, waitTime);
+                    // Delay other packets in the queue that arrive during backoff
                     for (int j = 1; j < transmittingNode.size(); j++) {
-                        if(transmittingNode.get(j) < delta){
-                            transmittingNode.set(j, delta);
-                            transmissionAttempts++;
+                        if(transmittingNode.get(j) < waitTime){
+                            transmittingNode.set(j, waitTime);
                         }
                         else{
                             break;
                         }
                     }
                 } else {
+                    // Collision count exceeds limit; drop packet and reset
                     collisionCounters[currNode] = 0;
                     transmittingNode.remove(0);
                     droppedCount++;
@@ -115,6 +120,7 @@ public class lab2 {
                     int delta = Math.abs(i-currNode);
                     double busyTime = currentTime + delta*(tProp) + tTrans;
                     for (int j = 0; j < currentNode.size(); j++) {
+                    	// if node senses line is busy, delay transmission
                         if(currentNode.get(j) < busyTime){
                             currentNode.set(j, busyTime);
                         }
@@ -133,15 +139,146 @@ public class lab2 {
         System.out.println(efficiency);
     }
 
-    // public static double[] nonPerstsitance(double a, double t, double r, double l, double d , double s) {
+    public static void nonPersistant(double a, double t, double r, double l, double d , double s,int n) {
 
-    // }
+
+        // List of nodes, where each node is a queue of packets
+        ArrayList<ArrayList<Double>> nodes = new ArrayList<ArrayList<Double>>(n); 
+
+        // For each node, generate arrival packets
+        for(int i = 0; i < n; i++){
+            ArrayList<Double> node = new ArrayList<Double>();
+            double currentTime = 0.0;
+            while (currentTime < t) {
+                currentTime += generateRandomAlpha(a);
+                node.add(currentTime);
+            }
+            nodes.add(node);
+        }
+
+        int[] collisionCounters = new int[n];		// Collision counter for each node
+        int[] collisionCounterSensing = new int[n];	// Collision counter for busybusy
+        int successCount = 0;					// Count of successfully transmitted packets
+        int droppedCount = 0;					// Count of dropped packets
+        int transmissionAttempts = 0;			// count of attemped transmissions
+        double tProp = d/s;						// Propegation delay between two adjacent nodes
+        double tTrans = l/r;					// Transmission delay for a packet
+        double bitTime = (512.0)/r;				// bit time
+        double currentTime = 0.0;				// current simulation time
+        while(true){
+            int currNode = getNextNode(nodes,n); 
+            if(currNode == -1) break;
+            boolean collisionDetected = false;
+            transmissionAttempts++;
+            ArrayList<Double> transmittingNode = nodes.get(currNode); // Node sending a packet
+            currentTime = transmittingNode.get(0);	// timestamp of next packet
+
+            // Loop through nodes to detect collisions
+            for(int i = 0; i < n; i++){
+                ArrayList<Double> currentNode = nodes.get(i);
+                if(i == currNode || currentNode.isEmpty()) continue;
+                int delta = Math.abs(i-currNode); // distance between transmitting node and current node
+                double dangertime = currentTime + delta*(tProp);
+
+                // Check for collision
+                if(currentNode.get(0) < dangertime){
+                    collisionCounters[i]++;
+                    double backOffTime = bitTime * generateRandomBackoff(collisionCounters[i]);
+                    
+                    //Check if collision count exceeds limit
+                    if(collisionCounters[i]<=10){
+                        collisionDetected = true;
+                        transmissionAttempts++;
+                        currentNode.set(0, (currentTime + backOffTime));
+                        
+                        // Delay other packets in the queue that arrive during backoff
+                        for (int j = 1; j < currentNode.size(); j++) {
+                            if(currentNode.get(j) < (currentTime + backOffTime)){
+                                currentNode.set(j, (currentTime + backOffTime));
+                            }
+                            else{
+                                break;
+                            }
+                        }
+                    } else {
+                    	// Collision count exceeds limit; drop packet and reset
+                        collisionCounters[i] = 0;
+                        currentNode.remove(0);
+                        droppedCount++;
+                    }
+                }
+            }
+
+            // Handle collision
+            if(collisionDetected){
+                collisionCounters[currNode]++;	//increment counter
+                if(collisionCounters[currNode] < 10){
+                	double backOffTime = bitTime * generateRandomBackoff(collisionCounters[currNode]); // set backoff
+                	double waitTime = currentTime + backOffTime;
+                    transmittingNode.set(0, waitTime);
+                    // Delay other packets in the queue that arrive during backoff
+                    for (int j = 1; j < transmittingNode.size(); j++) {
+                        if(transmittingNode.get(j) < waitTime){
+                            transmittingNode.set(j, waitTime);
+                        }
+                        else{
+                            break;
+                        }
+                    }
+                } else {
+                    // Collision count exceeds limit; drop packet and reset
+                    collisionCounters[currNode] = 0;
+                    transmittingNode.remove(0);
+                    droppedCount++;
+                }
+            } else{
+                collisionCounters[currNode] = 0;
+                transmittingNode.remove(0);
+                successCount++;
+
+                for(int i = 0; i < n; i++) {
+                	ArrayList<Double> currentNode = nodes.get(i);
+                    if(currentNode.isEmpty()) continue;
+                    int delta = Math.abs(i-currNode);
+                    double busyTime = currentTime + delta*(tProp) + tTrans;
+                    for (int j = 0; j < currentNode.size(); j++) {
+                    	// if node senses line is busy, delay transmission
+                        if(currentNode.get(j) < busyTime){
+                            collisionCounterSensing[currNode]++;
+                        	if(collisionCounterSensing[currNode] < 10){
+                				double backOffTime = bitTime * generateRandomBackoff(collisionCounterSensing[currNode]); // set backoff
+                        		double newDeparture = currentNode.get(j) + backOffTime;
+                            	currentNode.set(j, newDeparture);
+                        	}
+                        	else
+                        	{
+                        		collisionCounterSensing[currNode] = 0;
+                    			currentNode.remove(0);
+                    			droppedCount++;
+                        	}
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.printf("Dropped = %d\n", droppedCount);
+        System.out.printf("transmissionAttempts = %d\n", transmissionAttempts);
+        System.out.printf("successCount = %d\n", successCount);
+        double efficiency = ((double)successCount/((double)transmissionAttempts));
+        System.out.println(efficiency);
+
+    }
 
     public static double generateRandomAlpha(double lambda) {
         return -Math.log(1.0 - Math.random()) / lambda;
     }
     
-    public static int generateRandomBackoff(int upper) {
+    public static int generateRandomBackoff(int count) {
+    	int upper = (int)Math.pow(2,count);
         Random rand = new Random();
         int randomNum = rand.nextInt(upper);
         return randomNum;
